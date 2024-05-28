7454ec1fd28ba5b98be90801c26986c09f385b48
vypham <uty.tra@thebargainvillage.com>
2024-05-28 17:11:39 +0700

    message for format pattern

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 # config.py
 import toml
+import glob
+import os
 from supabase import create_client, Client
 from datetime import datetime, timedelta
+import unicodedata
 
 class Config:
     def __init__(self, config_path="config.toml"):
@@ -29,11 +32,35 @@ class Config:
 
     def get_selenium_config(self):
         selenium_config = self.config.get("selenium", {})
-        return selenium_config.get("username"), selenium_config.get("password"), selenium_config.get("chrome_options", [])
+        return selenium_config.get("chrome_options", [])
 
     def get_paths_config(self):
         paths_config = self.config.get("paths", {})
         return paths_config.get("extension_path", "")
 
+    def get_smartscount(self):
+        smartscount_config = self.config.get("smartscount", {})
+        return smartscount_config["username"], smartscount_config["password"]
+
+
+# Utility function to format headers
+def format_header(header):
+    # Convert to lowercase
+    header = header.lower()
+    # Replace spaces with underscores
+    header = header.replace(" ", "_")
+    # Remove Vietnamese characters by decomposing and keeping only ASCII
+    header = (
+        unicodedata.normalize("NFKD", header).encode("ASCII", "ignore").decode("ASCII")
+    )
+    return header
+
+# Utility function to get the newest file in a directory
+def get_newest_file(directory):
+    files = glob.glob(os.path.join(directory, "*"))
+    if not files:  # Check if the files list is empty
+        return None
+    newest_file = max(files, key=os.path.getmtime)
+    return newest_file
 # Initialize the configuration
 config = Config()
---
+++ ultis_get_searchterm_smartsount.py
@@ -0,0 +1,183 @@
+# main.py
+import os
+import tempfile
+import time
+import pandas as pd
+import psycopg2
+import psycopg2.extras
+from datetime import datetime, timedelta
+from supabase import create_client, Client
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
+from webdriver_manager.chrome import ChromeDriverManager
+import traceback
+from config import config, format_header, get_newest_file
+
+# Initialize Supabase client
+supabase = config.supabase
+
+# Get timezone offset and calculate current time in GMT+7
+current_time_gmt7 = config.current_time_gmt7
+
+# Get Selenium configuration
+chrome_options_list = config.get_selenium_config()
+
+# Get paths configuration
+extension_path = config.get_paths_config()
+
+# Create a temporary directory for downloads
+with tempfile.TemporaryDirectory() as download_dir:
+    # Chrome options
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {
+        "download.default_directory": download_dir,
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing.enabled": True,
+    }
+    chrome_options.add_experimental_option("prefs", prefs)
+
+    for option in chrome_options_list:
+        chrome_options.add_argument(option)
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    chrome_options.add_extension(os.path.join(dir_path, extension_path))
+
+
+def scrap_data_smartcount_relevant_product(driver, asin):
+    print("searchterm")
+    wait = WebDriverWait(driver, 30)
+    try:
+        print("scroll")
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="navSearchTerms"]')
+            )  # Replace "element_id" with the actual ID of the element
+        )
+        driver.execute_script("arguments[0].scrollIntoView();", element)
+        print("searchtermbutton")
+        searchterm_button = wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="navSearchTerms"]'))
+        )
+        print("searchtermbutton_click")
+        searchterm_button.click()
+        time.sleep(2)
+        print("asininput")
+        asin_input = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, '.mat-form-field-infix input[formcontrolname="asin"]')
+            )
+        )
+        # You can also set the maximum value if needed
+        asin_input.clear()
+        asin_input.send_keys(asin)
+        time.sleep(12)
+        print("searchbutton")
+        search_button = wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="btnSearchProducts"]'))
+        )
+        search_button.click()
+        time.sleep(2)
+        # Find the "Products" element using CSS Selector
+        print("relevant_products_button")
+        relevant_products_button = wait.until(
+            EC.visibility_of_element_located(
+                (
+                    By.XPATH,
+                    "//div[contains(@class, 'fixed-tab') and contains(text(), 'Relevant Products')]",
+                )
+            )
+        )
+        # Click on the "Products" element
+        relevant_products_button.click()
+        time.sleep(5)
+        # Wait for the button to be clickable
+        wait = WebDriverWait(driver, 10)
+        print("excel_button")
+        excel_button = wait.until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//span[text()='Excel']/ancestor::button")
+            )
+        )
+
+        # Click the "Excel" button
+        excel_button.click()
+        print("image")
+        image = wait.until(
+            EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, 'img[mattooltip="Export as CSV"]')
+            )
+        )
+
+        # Click the image
+        image.click()
+        time.sleep(10)
+        print("newest_file")
+
+        file_path = download_dir
+
+        newest_file_path = get_newest_file(file_path)
+
+        if newest_file_path:
+            data = pd.read_csv(newest_file_path)
+            data["sys_run_date"] = current_time_gmt7.strftime("%Y-%m-%d")
+            # Proceed with the database insertion
+        else:
+            print("No files found in the specified directory.")
+
+        # Extract the header row
+        headers = [
+            "amazon_image",
+            "asin_relevant",
+            "title",
+            "brand",
+            "common_search_terms",
+            "relevancy_score",
+            "sys_run_date",
+        ]
+
+        integer_columns = [
+            "relevancy_score",
+        ]
+        data = data.drop(data.columns[0], axis=1)
+        # Concatenate the URL with the data in the second column
+        data.rename(columns={data.columns[0]: "amazon_image"}, inplace=True)
+        data[data.columns[0]] = (
+            "https://images-na.ssl-images-amazon.com/images/I/"
+            + data[data.columns[0]].astype(str)
+        )
+        data.columns = headers
+        data.insert(0, "asin", "")
+        for col in integer_columns:
+            data[format_header(col)] = (
+                data[format_header(col)].astype(float).fillna(0.00)
+            )
+
+        print(data.head())
+        try:
+            # Convert rows to list of dictionaries and handle NaN values
+            rows_list = data.replace({np.nan: None}).to_dict(orient="records")
+
+            # Generate MD5 hash as the primary key for each row
+            for row_dict in rows_list:
+                row_dict["asin"] = str(asin)
+
+            # Insert the rows into the database using executemany
+            response = (
+                supabase.table("products_relevant_smartscounts")
+                .upsert(rows_list)
+                .execute()
+            )
+            if hasattr(response, "error") and response.error is not None:
+                raise Exception(f"Error inserting rows: {response.error}")
+            print(f"Rows inserted successfully")
+        except Exception as e:
+            print(f"Error with rows: {e}")
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
